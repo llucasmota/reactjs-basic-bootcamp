8445fbb96f35765db54d6b61691653f6e8a3756b
llucasmota <lucas.o.mota@gmail.com>
2020-02-04 07:52:44 -0300

    Adicionando loader de imagem e css

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./App.css";
+import profile from "./assets/mee.jpg";
+
+function App() {
+  return <img src={profile} width="400px" height="400px" alt="meu profile" />;
+}
+
+export default App;
