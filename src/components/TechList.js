b6430542327c17ea9cf16d1929cd8446a486a338
llucasmota <lucas.o.mota@gmail.com>
2020-02-05 13:12:02 -0300

    Adicionando métodos para alterar estado da lista

--- src/components/TechList.js
+++ src/components/TechList.js
@@ -2,16 +2,36 @@ import React, { Component } from "react";
 
 class Techlist extends Component {
   state = {
+    newTech: "",
     techs: ["NodeJS", "ReactJS", "React Native"]
   };
+  handleInputChange = e => {
+    this.setState({ newTech: e.target.value });
+  };
+
+  handleSubmit = e => {
+    //previne o recarregamento da view
+    e.preventDefault();
+
+    this.setState({ techs: [...this.state.techs, this.state.newTech] });
+  };
+
   render() {
     console.log(this.state);
     return (
-      <ul>
-        <li>NodeJS</li>
-        <li>ReactJS</li>
-        <li>MongoDB</li>
-      </ul>
+      <form onSubmit={this.handleSubmit}>
+        <ul>
+          {this.state.techs.map(tech => (
+            <li key={tech}>{tech}</li>
+          ))}
+        </ul>
+        <input
+          type="text"
+          onChange={this.handleInputChange}
+          value={this.state.newTech}
+        />
+        <button type="submit">Enviar</button>
+      </form>
     );
   }
 }
